df5c404b88ce23221bbdfba753b76da420f30dc0
viveroa2291 <viveroa2291@uwec.edu>
2023-10-05 23:26:30 -0500

    Home Page Additions
    Added some more onto the home page.
    
    TODO:
    - Clean up the UI
    - clean up the code
    - Add more features.

--- home.js
+++ home.js
@@ -9,3 +9,15 @@ function map() {
     listview.style.display = "none";
     mapview.style.display = "block"; 
 }
+function arizona() {
+    document.location.href="arizona.html";
+}
+function arkansas() {
+    document.location.href="arkansas.html";
+}
+function california() {
+    document.location.href="california.html";
+}
+function florida() {
+    document.location.href="florida.html";
+}
